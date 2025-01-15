d26c27395b993b6f70489017601fc9d68a8e4f48
shakshishar <sakshisharma1495@gmail.com>
2025-01-15 18:17:39 +0530

    added header

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,30 @@
 const Header=()=>{
     return(
-        <></>
+       
+        <div className="flex items-center justify-between p-3">
+            {/* the logo part */}
+            <div className="flex item-center">
+                <img src="null" alt=""></img>
+            </div>
+
+            {/* the menus */}
+            <div>
+                <ul className="flex space-x-8">
+                    <li>Home</li>
+                    <li>Explore more</li>
+                    <li>Contact Us</li>
+                    <li>About</li>
+                </ul>
+
+            </div>
+
+            {/* the profile */}
+            <div>
+                <button className="px-4 py-2 text-black">
+
+                </button>
+            </div>
+        </div>
     )
 }
 
